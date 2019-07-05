cd0b8fd60fd8785928bde4d6d921542af476cea6
Joe Noel <noel1230071@gmail.com>
2019-07-06 02:33:49 +0530

    Update problem-36.py

--- Pro/Set-4/problem-36.py
+++ Pro/Set-4/problem-36.py
@@ -1,9 +1,10 @@
+#update
 n = int(input())
 power = list(map(int,input().split()))
 countt = 0
-for i in range(n-2):
-    for j in range(i+1,n-1):
-        for k in range(j+1,n):
+for i in range(len(power)-2):
+    for j in range(i+1,len(power)-1):
+        for k in range(j+1,len(power)):
             if power[i] > power[j]:
                 if power[j] > power[k]:
                     countt += 1
