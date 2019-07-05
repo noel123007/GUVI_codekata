91dd5aef5372f8c698024080635c0810170a68f8
Joe Noel <noel1230071@gmail.com>
2019-07-05 11:47:22 +0530

    Update problem-2.py

--- Pro/Set-1/problem-2.py
+++ Pro/Set-1/problem-2.py
@@ -1,14 +1,14 @@
-n, k = input().split()
-k = int(k)
-s = ''
-if k == 0:
-  print(int(n))
-elif k == len(n):
+n1, k1 = input().split()
+k1 = int(k1)
+s1 = ''
+if k1 == 0:
+  print(int(n1))
+elif k1 == len(n1):
   print(0)
 else:
-  n = list(map(int, list(str(n))))
-  for i in range(len(n) - k):
-    s = s + str(min(n[i:k+1]))
-    n.remove(min(n[i:k+1]))
-    print(n)
-  print(s)
+  n1 = list(map(int, list(str(n1))))
+  for i1 in range(len(n1) - k1):
+    s1 = s1 + str(min(n1[i1:k1+1]))
+    n1.remove(min(n1[i1:k1+1]))
+#     print(n)
+  print(s1)
