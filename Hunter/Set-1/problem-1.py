d497f39a70229a91ce0739c1bda121ad98b17011
Joe Noel <noel1230071@gmail.com>
2019-07-06 03:06:57 +0530

    Create problem-1.py

---
+++ Hunter/Set-1/problem-1.py
@@ -0,0 +1,10 @@
+n = int(input())
+num = input().split()
+temp = []
+
+for i in num:
+    if num.count(i) > 1 and i not in temp:
+        temp.append(i)
+temp = sorted(temp)
+for i in temp:
+    print(int(i),end=' ')
