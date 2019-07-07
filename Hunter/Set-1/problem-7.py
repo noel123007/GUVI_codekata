f64807a44de3873923a64104600f876c7bb1533a
Joe Noel <noel1230071@gmail.com>
2019-07-08 02:13:28 +0530

    Create problem-7.py

---
+++ Hunter/Set-1/problem-7.py
@@ -0,0 +1,8 @@
+n = int(input())
+l = list(map(int,input().split()))
+
+for i in range(len(l)):
+    if i % 2 == 0 and l[i] % 2 == 1:
+        print(l[i],end =' ')
+    if i % 2 == 1 and l[i] % 2 == 0:
+        print(l[i],end =' ')
